026d7bdc3ee133b363d358af3761a266fb2b2b74
martaenciso <m.enciso@latrobe.edu.au>
2015-03-05 13:54:33 +1100

    New structure of the program that will allow multiplication Just sums at this stage

--- calc.py
+++ calc.py
@@ -1,4 +1,10 @@
 import sys
 
+def add_all(nums):
+    return sum(nums)
+
 if __name__ == '__main__':
-   print(sum(map(float, sys.argv[1:])))
+    command = sys.argv[1]
+    nums = map(float, sys.argv[2:])
+    if command == 'add':
+        print(add_all(nums))
