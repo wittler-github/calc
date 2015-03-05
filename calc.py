cc09868cd9c090c022b1accd830c2cba986a046e
martaenciso <m.enciso@latrobe.edu.au>
2015-03-05 14:00:01 +1100

    Allows sums and multiplication of floats

--- calc.py
+++ calc.py
@@ -3,8 +3,14 @@ import sys
 def add_all(nums):
     return sum(nums)
 
+def multiply_all(nums):
+    return reduce(lambda a, b: a * b, nums)
+
+
 if __name__ == '__main__':
     command = sys.argv[1]
     nums = map(float, sys.argv[2:])
     if command == 'add':
         print(add_all(nums))
+    elif command == 'multiply':
+        print(multiply_all(nums))
