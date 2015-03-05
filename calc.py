7d64cead5967a97dd4f918e1795c5b7c411fe00a
Henry Wittler <17526019@students.latrobe.edu.au>
2015-03-05 15:16:51 +1100

    1 max commit

--- calc.py
+++ calc.py
@@ -1,5 +1,4 @@
 """calc.py: A simple Python calculator."""
-
 import sys
 
 def add_all(nums):
@@ -7,7 +6,6 @@ def add_all(nums):
 
 def multiply_all(nums):
     return reduce(lambda a, b: a * b, nums)
-
 
 if __name__ == '__main__':
     command = sys.argv[1]
@@ -18,6 +16,8 @@ if __name__ == '__main__':
         print(multiply_all(nums))
     elif command == 'min':
         print(min(nums))
+    elif command == 'max':
+        print(max(nums))
     else:
         usage = "calc.py [add|multiply|min] NUM1 [NUM2 [NUM3 [...]]]"
         print(usage)
