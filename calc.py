d63eec8ec53b4a20d4ac5149b7eb7cfa2d094e2d
martaenciso <m.enciso@latrobe.edu.au>
2015-03-05 14:09:37 +1100

    Add usage message if instruction not recognized

--- calc.py
+++ calc.py
@@ -16,3 +16,6 @@ if __name__ == '__main__':
         print(add_all(nums))
     elif command == 'multiply':
         print(multiply_all(nums))
+    else:
+        usage = "calc.py [add|multiply] NUM1 [NUM2 [NUM3 [...]]]"
+        print(usage)
